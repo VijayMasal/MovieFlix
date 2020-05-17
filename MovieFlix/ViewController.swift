95d3ab4825925109f6cc4fd164911ac83b873821
Vijay Masal <vdmasal@gmail.com>
2020-05-17 09:01:43 +0530

    Initial Commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Vijay Masal on 17/05/20.
+//  Copyright © 2020 Vijay Masal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
